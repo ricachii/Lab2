abd95024e19b404bc00ff9607be634ebd13ef3d1
Ark3ik <gabriel.eduardo.20012005@gmail.com>
2025-06-09 19:34:32 -0400

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -93,6 +93,22 @@ void mergeSort(vector<int32_t>& arr, int left, int right) {
     }
 }
 
+void radixSort(vector<int32_t>& arr) {
+    if (arr.empty()) return;
+    int32_t maxVal = *max_element(arr.begin(), arr.end());
+    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
+        vector<int32_t> output(arr.size());
+        int count[10] = {0};
+        for (int i = 0; i < arr.size(); i++) count[(arr[i] / exp) % 10]++;
+        for (int i = 1; i < 10; i++) count[i] += count[i - 1];
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        arr = output;
+    }
+}
+
 // ----------- FUNCIONES AUXILIARES -----------
 
 bool readBinaryFile(const string& filename, vector<int32_t>& data) {
